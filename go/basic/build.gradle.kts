ae30c78ae52b3a0498d35e5371e5e4c1f2677ec2
Bulgakov Alexander <buls@yandex.ru>
2021-09-03 22:18:05 +0300

    run go benchmarks fix

--- go/basic/build.gradle.kts
+++ go/basic/build.gradle.kts
@@ -1,11 +1,3 @@
-plugins {
-    id("com.github.blindpirate.gogradle") version "0.11.4"
-}
-
-golang {
-    packagePath = "."
-}
-
-tasks.create("goBenchmarks", com.github.blindpirate.gogradle.Go::class.java) {
-    go("test -bench=. ./...")
+tasks.create("goBenchmarks", Exec::class.java) {
+    commandLine("go", "test", "-bench", ".", "./...")
 }
