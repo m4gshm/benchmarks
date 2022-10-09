2d844fbc3cbb1b85d2804854d6276acd4c59fc88
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2022-10-09 14:36:26 +0300

    mingw support

--- rest/java/quarkus/record-jfr.sh
+++ rest/java/quarkus/record-jfr.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+IS_WIN=false
+case "`uname`" in
+  CYGWIN* )
+    IS_WIN=true
+    ;;
+  MSYS* | MINGW* )
+    IS_WIN=true
+    ;;
+esac
+
 SLEEP=2
 
 APP_PORT=8082
@@ -23,32 +33,38 @@ echo build application
 echo start application
 $APP_RUN &
 APP_PID=$!
-echo $APP_PID
+JCMD_APP_PID=$APP_PID
+if  $IS_WIN; then
+  JCMD_APP_PID=$(cat /proc/$APP_PID/winpid)
+fi
+
+echo "APP PID  $APP_PID"
+echo "JCMD PID $JCMD_APP_PID"
 
 sleep $SLEEP
 
 WARM_CYCLES=4
 for ((i=1;i<=WARM_CYCLES;i++)); do
   echo "warmup $i"
-  REC_ID=$(jcmd $APP_PID JFR.start duration=$REC_DURATION filename=/tmp/ settings=$REC_PROFILE | grep "Started recording " | awk {'print $3'} | tr --delete '.')
+  REC_ID=$(jcmd $JCMD_APP_PID JFR.start duration=$REC_DURATION filename=/tmp/ settings=$REC_PROFILE | grep "Started recording " | awk {'print $3'} | tr --delete '.')
   echo "rec id $REC_ID"
 
   $K6_RUN
 
-  jcmd $APP_PID JFR.stop name=$REC_ID
+  jcmd $JCMD_APP_PID JFR.stop name=$REC_ID
 done
 
 REC_CYCLES=2
 for ((i=1;i<=REC_CYCLES;i++)); do
   echo "start recording $i"
 
-  REC_ID=$(jcmd $APP_PID JFR.start duration=$REC_DURATION filename=$REC_OUT settings=$REC_PROFILE | grep "Started recording " | awk {'print $3'} | tr --delete '.')
+  REC_ID=$(jcmd $JCMD_APP_PID JFR.start duration=$REC_DURATION filename=$REC_OUT settings=$REC_PROFILE | grep "Started recording " | awk {'print $3'} | tr --delete '.')
   echo "rec id $REC_ID"
 
   $K6_RUN
 
   echo "stop recording $i"
-  jcmd $APP_PID JFR.stop name=$REC_ID
+  jcmd $JCMD_APP_PID JFR.stop name=$REC_ID
 done
 
 echo finish application process $APP_PID
