a5476925d02567cde6761fef6f660f15397dc8e6
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2022-10-23 21:58:01 +0300

    gorm bench fix

--- rest/go/run-k6-no-trace-gorm.sh
+++ rest/go/run-k6-no-trace-gorm.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker restart postgres
-# docker stop postgres
-# docker rm -f postgres
-# docker run -d --restart always --name postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:14.3 postgres -c max_connections=1000
-# sleep 3
+#docker restart postgres
+ docker stop postgres
+ docker rm -f postgres
+ docker run -d --restart always --name postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:14.3 postgres -c max_connections=1000
+ sleep 3
 
 WRITE_TRACE=false K6_ITERATIONS=6000 GOMAXPROCS=2 ./run-k6.sh -storage gorm -migrate-db -sql-log-level silent -max-db-conns 400 -max-db-idle-conns 400
