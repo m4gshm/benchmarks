c454c361bb9093ff91df8a9e39df7077a900f78c
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2022-10-28 01:07:45 +0300

    run benchmarks without trace recording

---
+++ rest/java/mvc/run-k6-no-jfr.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+WRITE_TRACE=false ./run-k6.sh
--- rest/java/quarkus/run-k6-no-jfr-db.sh
+++ rest/java/quarkus/run-k6-no-jfr-db.sh
@@ -3,4 +3,4 @@
 docker rm -f postgres
 docker run -d --restart always --name postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:14.3
 
-WRITE_TRACE=false K6_ITERATIONS=6000 ./run-k6.sh -Dstorage=db
+WRITE_TRACE=false K6_ITERATIONS=6000 QUARKUS_DATASOURCE_JDBC_URL=jdbc:postgresql://localhost:5432/postgres ./run-k6.sh -Dstorage=db
