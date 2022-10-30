5d8e1b08a08984e1c571c89ce29ae8ed0999f1a2
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2022-10-30 14:34:21 +0300

    don't restart docker postgres container for running of in-memory storage based spring-mvc application

--- rest/java/webflux/run-k6-no-jfr.sh
+++ rest/java/webflux/run-k6-no-jfr.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-docker rm -f postgres
-docker run -d --restart always --name postgres -p 5432:5432 -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres postgres:14.3
-
 WRITE_TRACE=false SPRING_DATASOURCE_ENABLED=false ./run-k6.sh
