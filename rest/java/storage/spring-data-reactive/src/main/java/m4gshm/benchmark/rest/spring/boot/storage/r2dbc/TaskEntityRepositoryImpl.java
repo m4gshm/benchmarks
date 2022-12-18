b99ded2bea1e6765f25a24237698cc599a29a4a1
Bulgakov Alexander <mfourgeneralsherman@gmail.com>
2022-12-18 23:09:15 +0300

    webflux + clear r2dbc draft

--- rest/java/storage/spring-data-reactive/src/main/java/m4gshm/benchmark/rest/spring/boot/storage/r2dbc/TaskEntityRepositoryImpl.java
+++ rest/java/storage/spring-data-reactive/src/main/java/m4gshm/benchmark/rest/spring/boot/storage/r2dbc/TaskEntityRepositoryImpl.java
@@ -27,23 +27,24 @@ public class TaskEntityRepositoryImpl implements TaskEntityRepository<TaskEntity
 
     @Override
     public Mono<TaskEntity> findById(String id) {
-        return conn(false).flatMapMany(connection -> bind(connection.createStatement("SELECT " +
-                        TaskEntity.TASK_COLUMN_TEXT + "," +
-                        TaskEntity.TASK_COLUMN_DEADLINE +
-                        " FROM " + TaskEntity.TABLE_NAME_TASK +
-                        " WHERE " + TaskEntity.TASK_COLUMN_ID + " = $1"
-                ), "$1", id, String.class).fetchSize(1).execute()
-        ).flatMap(result -> result.map((row, rowMetadata) -> TaskEntity.builder()
-                .id(id)
-                .text(row.get(TaskEntity.TASK_COLUMN_TEXT, String.class))
-                .deadline(row.get(TaskEntity.TASK_COLUMN_DEADLINE, LocalDateTime.class))
-                .build())
-        ).next();
+        return connMono(false, connection -> Flux.from(bind(connection.createStatement(
+                        "SELECT " +
+                                TaskEntity.TASK_COLUMN_TEXT + "," +
+                                TaskEntity.TASK_COLUMN_DEADLINE +
+                                " FROM " + TaskEntity.TABLE_NAME_TASK +
+                                " WHERE " + TaskEntity.TASK_COLUMN_ID + " = $1"
+                ), "$1", id, String.class).fetchSize(1).execute())
+                .flatMap(result -> result.map((row, rowMetadata) -> TaskEntity.builder()
+                        .id(id)
+                        .text(row.get(TaskEntity.TASK_COLUMN_TEXT, String.class))
+                        .deadline(row.get(TaskEntity.TASK_COLUMN_DEADLINE, LocalDateTime.class))
+                        .build()))
+                .next());
     }
 
     @Override
     public Mono<TaskEntity> save(TaskEntity entity) {
-        return inTrans(connection -> {
+        return transactMono(connection -> {
             var deadline = entity.getDeadline();
             var text = entity.getText();
             var statement = connection.createStatement("INSERT INTO " +
@@ -69,25 +70,24 @@ public class TaskEntityRepositoryImpl implements TaskEntityRepository<TaskEntity
 
     @Override
     public Mono<? extends Number> deleteById(String id) {
-        return conn(true).flatMapMany(connection -> bind(connection.createStatement(
-                        "DELETE FROM " + TaskEntity.TABLE_NAME_TASK + " WHERE " + TaskEntity.TASK_COLUMN_ID + "=$1"
-                ), "$1", id, String.class).execute()).flatMap(Result::getRowsUpdated).next();
+        return connMono(true, connection -> Flux.<Result>from(bind(connection.createStatement(
+                "DELETE FROM " + TaskEntity.TABLE_NAME_TASK + " WHERE " + TaskEntity.TASK_COLUMN_ID + "=$1"
+        ), "$1", id, String.class).execute()).flatMap(Result::getRowsUpdated).next().map(n -> (Number) n));
     }
 
     @Override
     public Flux<TaskEntity> findAll() {
-        return conn(false).flatMapMany(connection -> connection.createStatement("SELECT " +
-                        TaskEntity.TASK_COLUMN_ID + "," +
-                        TaskEntity.TASK_COLUMN_TEXT + "," +
-                        TaskEntity.TASK_COLUMN_DEADLINE +
-                        " FROM " + TaskEntity.TABLE_NAME_TASK
-                ).execute()
-        ).flatMap(result -> result.map((row, rowMetadata) -> TaskEntity.builder()
+        return connFlux(false, connection -> Flux.from(connection.createStatement("SELECT " +
+                TaskEntity.TASK_COLUMN_ID + "," +
+                TaskEntity.TASK_COLUMN_TEXT + "," +
+                TaskEntity.TASK_COLUMN_DEADLINE +
+                " FROM " + TaskEntity.TABLE_NAME_TASK
+        ).execute()).flatMap(result -> result.map((row, rowMetadata) -> TaskEntity.builder()
                 .id(row.get(TaskEntity.TASK_COLUMN_ID, String.class))
                 .text(row.get(TaskEntity.TASK_COLUMN_TEXT, String.class))
                 .deadline(row.get(TaskEntity.TASK_COLUMN_DEADLINE, LocalDateTime.class))
                 .build())
-        );
+        ));
     }
 
     @NotNull
@@ -98,20 +98,23 @@ public class TaskEntityRepositoryImpl implements TaskEntityRepository<TaskEntity
 
     @NotNull
     private Mono<? extends Connection> conn() {
-        return from(connectionFactory.create());
+        return Mono.from(connectionFactory.create());
     }
 
     @NotNull
-    private <T> Mono<T> conn(boolean autoCommit, Function<Connection, Mono<T>> routine) {
-        return conn(autoCommit).flatMap(routine);
+    private <T> Mono<T> connMono(boolean autoCommit, Function<Connection, Mono<T>> routine) {
+        return Mono.usingWhen(conn(autoCommit), routine, Connection::close);
     }
 
     @NotNull
-    private <T> Mono<T> inTrans(Function<Connection, Mono<T>> routine) {
-        return conn().flatMap(c ->
-                from(c.beginTransaction())
-                        .thenReturn(c).flatMap(routine)
-                        .flatMap(t -> from(c.commitTransaction()).thenReturn(t))
-        );
+    private <T> Mono<T> transactMono(Function<Connection, Mono<T>> routine) {
+        return connMono(false, c -> from(c.beginTransaction())
+                .then(routine.apply(c))
+                .flatMap(t -> from(c.commitTransaction()).thenReturn(t)));
+    }
+
+    @NotNull
+    private <T> Flux<T> connFlux(boolean autoCommit, Function<Connection, Flux<T>> routine) {
+        return Flux.usingWhen(conn(autoCommit), routine, Connection::close);
     }
 }
