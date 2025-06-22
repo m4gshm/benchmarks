f13d99784ef82458def3f93ca90bbde431068ce5
Alexander Bulgakov <mfourgeneralsherman@gmail.com>
2025-06-22 12:14:58 +0300

    join delete queries

--- rest/java/storage/jdbc/src/main/java/m4gshm/benchmark/rest/spring/boot/storage/jdbc/TaskStorageJdbcImpl.java
+++ rest/java/storage/jdbc/src/main/java/m4gshm/benchmark/rest/spring/boot/storage/jdbc/TaskStorageJdbcImpl.java
@@ -241,13 +241,11 @@ public class TaskStorageJdbcImpl implements Storage<TaskImpl, String> {
         try (var connection = dataSource.getConnection()) {
             try {
                 connection.setAutoCommit(false);
-                try (var statement = connection.prepareStatement(query.SQL_TASK_TAG_DELETE_BY_TASK_ID)) {
-                    statement.setString(1, id);
-                    statement.execute();
-                }
+                var sql = query.SQL_TASK_TAG_DELETE_BY_TASK_ID + ";" + query.SQL_TASK_DELETE_BY_ID;
                 int deletedCount;
-                try (var statement = connection.prepareStatement(query.SQL_TASK_DELETE_BY_ID)) {
+                try (var statement = connection.prepareStatement(sql)) {
                     statement.setString(1, id);
+                    statement.setString(2, id);
                     deletedCount = statement.executeUpdate();
                 }
                 connection.commit();
